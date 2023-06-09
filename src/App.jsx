3b32c625fda36e951660af3c495f2a6b1472c98e
Maylon-tech <minatokiroisen77@gmail.com>
2023-06-09 14:33:12 +0900

    starting doing api fetch

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Pages from './pages/Pages'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Recipes API here
+      </header>
+      <Pages />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Popular.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+const Popular = () => {
+    const [popular, setPopular] = useState([])
+
+    useEffect(() => {
+        getPopular()
+    }, [])
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+        const data = await api.json()
+        console.log(data.recipes)
+        setPopular(data.recipes)
+
+    }
+
+
+
+  return (
+    <div>
+        {
+            popular.map((recipe) => {
+                return (
+                    <div>
+                        <p>{recipe.title}</p>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default Popular
